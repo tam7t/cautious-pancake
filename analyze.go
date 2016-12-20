5aec674c166457816e234f4ec2764e6675a696b2
Tommy Murphy <tommy.murphy@gmail.com>
2016-12-20 16:27:01 -0500

    refactor: split up analyzing to funcs on a struct

--- analyze.go
+++ analyze.go
@@ -10,81 +10,91 @@ import (
 	"golang.org/x/tools/go/ssa/ssautil"
 )
 
-func Analyze(iprog *loader.Program) (map[*ssa.Function]bool, error) {
-	// Create and build SSA-form program representation.
+// CallGraph keeps track of 'pure' and function
+type CallGraph struct {
+	Prog      *loader.Program
+	CallGraph *callgraph.Graph
+	Mapping   map[*ssa.Function]bool
+}
+
+// NewCallGraph creates a CallGraph by performing ssa analysis
+func NewCallGraph(iprog *loader.Program) *CallGraph {
+	// Create and build SSA-form program representation
 	prog := ssautil.CreateProgram(iprog, 0)
 	prog.Build()
 
 	// cha to find dynamic calls
-	cg := cha.CallGraph(prog)
-	cg.DeleteSyntheticNodes()
+	cgo := cha.CallGraph(prog)
+	cgo.DeleteSyntheticNodes()
 
-	// mark functions in the callgraph that are not 'pure'
-	results, err := MarkImpure(cg)
-	if err != nil {
-		return nil, err
+	return &CallGraph{
+		Prog:      iprog,
+		CallGraph: cgo,
+		Mapping:   make(map[*ssa.Function]bool),
 	}
-
-	// filter output to only include functions imported by the program
-	filtered := make(map[*ssa.Function]bool)
-	for k, v := range results {
-		if k == nil {
-			continue
-		}
-		_, ok := iprog.Imported[k.Package().Pkg.Path()]
-		if ok && !v {
-			filtered[k] = v
-		}
-	}
-
-	return filtered, nil
 }
 
-// MarkImpure takes a callgraph and analyses the nodes looking for functions that modify
-// global state or functions that call functions that modify global state
-func MarkImpure(cg *callgraph.Graph) (map[*ssa.Function]bool, error) {
-	results := make(map[*ssa.Function]bool)
-
+// Analyze examines the callgraph looking for functions that modify global
+// state or call functions that modify global state
+func (cg *CallGraph) Analyze() error {
 	// basic analysis
-	for fn, _ := range cg.Nodes {
-		results[fn] = accessGlobal(fn) || usesInterface(fn)
+	for fn, _ := range cg.CallGraph.Nodes {
+		cg.Mapping[fn] = accessGlobal(fn) || usesInterface(fn)
 	}
 
 	// sub function analysis, if a func calls an func that is not pure then it
 	// needs to be marked as such
-	if err := callgraph.GraphVisitEdges(cg, func(edge *callgraph.Edge) error {
+	if err := callgraph.GraphVisitEdges(cg.CallGraph, func(edge *callgraph.Edge) error {
 		// analyze callee (ssa.Function)
 		callee := edge.Callee.Func
 		caller := edge.Caller.Func
 
 		// callee modifies state, mark static callers bad as well
-		if results[callee] {
+		if cg.Mapping[callee] {
 			if dynamic(edge) {
 				if !includes(edge.Site.Common().Value, caller.Params) {
-					results[edge.Caller.Func] = true
+					cg.Mapping[edge.Caller.Func] = true
 				}
 			} else {
-				results[edge.Caller.Func] = true
+				cg.Mapping[edge.Caller.Func] = true
 			}
 		}
 
 		return nil
 	}); err != nil {
-		return nil, err
+		return err
 	}
 
-	return results, nil
+	return nil
 }
 
-// accessGlobal does a simple analysis of the ssa representation of a function to detect
-// whether a global variable is read or modified as part of the function
+// Pure returns a slice of the ssa representations of the pure functions in the
+// analyzed package
+func (cg *CallGraph) Pure() []*ssa.Function {
+	// filter output to only include functions imported by the program
+	var filtered []*ssa.Function
+
+	for k, v := range cg.Mapping {
+		if k == nil {
+			continue
+		}
+		_, ok := cg.Prog.Imported[k.Package().Pkg.Path()]
+		if ok && !v {
+			filtered = append(filtered, k)
+		}
+	}
+
+	return filtered
+}
+
+// accessGlobal does a simple analysis of the ssa representation of a function
+// to detect whether a global variable is read or modified
 func accessGlobal(f *ssa.Function) bool {
 	if f == nil {
-		// nil functions probably dont modify state?
+		// nil functions probably do not modify state?
 		return false
 	}
 
-	// check each instruction
 	for _, b := range f.Blocks {
 		for _, i := range b.Instrs {
 			switch v := i.(type) {
@@ -102,13 +112,15 @@ func accessGlobal(f *ssa.Function) bool {
 	return false
 }
 
-// eliminate functions that have interface inputs since we dont know how they will actually be implemented (and may depend on global state.
-// todo: check structs for iface parameters
+// usesInterface eliminates functions that accept an interface as a parameter
+// since it is difficult to resolve whether the interface's implementation will
+// modify global state
 func usesInterface(f *ssa.Function) bool {
 	if f == nil {
 		return false
 	}
 	for _, v := range f.Params {
+		// TODO: check structs for iface parameters
 		if types.IsInterface(v.Type()) {
 			return true
 		}
@@ -116,12 +128,12 @@ func usesInterface(f *ssa.Function) bool {
 	return false
 }
 
-// dynamic is a helper that checks whether the callgraph edge is a dynamic funciton call
+// dynamic is a helper that checks whether the callgraph edge is a dynamic call
 func dynamic(edge *callgraph.Edge) bool {
 	return edge.Site != nil && edge.Site.Common().StaticCallee() == nil
 }
 
-// includes checks whether the value (needle) is one of the parameters (hay) of a function
+// includes checks whether the value (needle) is a function's parameters (hay)
 func includes(needle ssa.Value, hay []*ssa.Parameter) bool {
 	n, ok := needle.(*ssa.Parameter)
 	if !ok {
