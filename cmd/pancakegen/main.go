0ab3f34f3934e340f7a11234ba45619920b8d1f8
Tommy Murphy <tommy.murphy@gmail.com>
2022-03-20 18:05:47 -0400

    feat: update pancakegen to use 1.18 fuzzing

--- cmd/pancakegen/main.go
+++ cmd/pancakegen/main.go
@@ -42,38 +42,19 @@ func main() {
 	}
 }
 
-const fuzzTemp = `package main
+const fuzzTemp = `package {{.Package.Pkg.Name}}
 
 import (
-	"fmt"
-	"runtime/debug"
-
-	"github.com/google/gofuzz"
-	"{{.Package.Pkg.Path}}"
+	"testing"
 )
 
-func main() { {{if gt (len .Params) 0}}{{if not (fuzznil .) }}
-	f := fuzz.New(){{end}}{{if fuzznil .}}{{if gt (len .Params) 1}}
-	f := fuzz.New()
-	{{end}}
-	f1 := fuzz.New().NilChance(0){{end}}{{end}}
-
-{{range $i, $v := .Params}}	var p{{$i}} {{$v.Type | strippkg}}
-{{end}}
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("%s\n", debug.Stack())
-			fmt.Println("found panic", r){{range $i, $v := .Params}}
-			fmt.Printf("p{{$i}}: %+v\n", p{{$i}}){{end}}
-		}
-	}()
-	for { {{range $i, $v := .Params}}
-		f{{if and (not $i) (fuzznil .)}}1{{end}}.Fuzz(&p{{$i}}){{end}}
-		{{if .Signature.Recv}}
+func Fuzz{{.Name}}(f *testing.F) {
+	f.Fuzz(func(t *testing.T, {{range $i, $v := .Params}}p{{$i}} {{$v.Type | strippkg}}{{end}}) { {{if .Signature.Recv}}
 		p0.{{.Name}}({{range $i, $v := .Params}}{{if $i}}{{if gt $i 1}}, {{end}}p{{$i}}{{end}}{{end}}{{if .Signature.Variadic}}...{{end}}){{else}}
-		{{.Package.Pkg.Name}}.{{.Name}}({{range $i, $v := .Params}}{{if $i}}, {{end}}p{{$i}}{{end}}{{if .Signature.Variadic}}...{{end}}){{end}}
-	}
-}`
+		{{.Name}}({{range $i, $v := .Params}}{{if $i}}, {{end}}p{{$i}}{{end}}{{if .Signature.Variadic}}...{{end}}){{end}}
+	})
+}
+`
 
 func PrintFuzz(f *ssa.Function) string {
 	var out bytes.Buffer
